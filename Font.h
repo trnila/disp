3f2f81fdc5774347571d9562f74c2474873d13af
Daniel Trnka <daniel.trnka@gmail.com>
2017-11-14 19:23:54 +0100

    Added forgotten file

---
+++ Font.h
@@ -0,0 +1,38 @@
+#pragma once
+
+class Font {
+public:
+	Font(unsigned char (*font)[8]): font(font) {
+		calculateLetterSizes();
+		this->sizes[' '] = 8;
+		this->starts[' '] = 1;
+	}
+
+	int sizes[255];
+	int starts[255];
+	unsigned char (*font)[8];
+
+private:
+
+
+	void calculateLetterSizes() {
+		for(int i = 0; i < 255; i++) {
+			int start = 0;
+			int stop = 7;
+
+			unsigned char *bitmap = font[i];
+			for(int j = 0; j < 8; j++) {
+				for(int row = 0; row < 8; row++) {
+					if(bitmap[row] & 1 << (j)) {
+						if(start == 0) {
+							start = j;
+						}
+						stop = j;
+					}
+				}
+			}
+			this->sizes[i] = stop + 2;
+			this->starts[i] = start;
+		}
+	};
+};
