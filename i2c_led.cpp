6f930ce85d00b396b934ae30d0b3599278fbf5a7
Daniel Trnka <daniel.trnka@gmail.com>
2017-11-14 20:04:13 +0100

    Template test

--- i2c_led.cpp
+++ i2c_led.cpp
@@ -9,6 +9,48 @@
 #include "Font.h"
 #include "algo.h"
 
+template<typename A, typename B>
+struct Renderer {
+	Renderer(A a, B b): a(a), b(b) {}
+
+	void operator()(Display &display) {
+		a.render(display);
+		b.render(display);
+	}
+
+	A a;
+	B b;
+};
+/*
+template<typename A>
+struct Renderer{
+	Renderer(A a): a(a) {
+
+	}
+
+	void operator()(Display &display) {
+		a.renderer(display);
+	}
+
+	A a;
+};
+
+template<typename A, typename... B>
+struct Renderer {
+	Renderer(A a, B... b): a(a), b(Renderer(b...)) {}
+
+	void operator()(Display &display) {
+		a.render(display);
+		b.render(display);
+	}
+
+	void render(Display &) {
+
+	}
+
+A a;
+Renderer b;
+};*/
 
 int main(int argc, char **argv) {
 	if(argc != 2) {
@@ -24,9 +66,12 @@ int main(int argc, char **argv) {
 	newInput(&input, text, &font);
 
 	Display display;
-	//I2CRenderer rendererA("/dev/i2c-2");
+	I2CRenderer rendererA("/dev/i2c-2");
 	OstreamRenderer renderer(std::cout);
 	BitsAlgo algo(&input);
+
+	//Renderer r(OstreamRenderer(std::cout), I2CRenderer("/dev/i2c-2"), OstreamRenderer(std::cerr));
+	Renderer r(OstreamRenderer(std::cout), I2CRenderer("/dev/i2c-2"));
 
 	for(;;) {
 		display.clear();
@@ -35,7 +80,8 @@ int main(int argc, char **argv) {
 		//display.rev();
 		//display.revCols();
 		//display.rotate(1);
-		renderer.render(display);
+		//renderer.render(display);
+		r(display);
 
 		usleep(50000);
 	}
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 i2c_led:
-	g++ -g -std=c99 i2c_led.cpp -o i2c_led
+	g++ -g -std=c++17 i2c_led.cpp -o i2c_led
 clean:
 	rm i2c_led
